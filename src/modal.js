250a68e6788b135d425dc7da53a83867875dd6e7
abrunhosa16 <146744014+abrunhosa16@users.noreply.github.com>
2023-11-05 17:47:45 +0000

    modal sem topbar stilos

---
+++ src/modal.js
@@ -0,0 +1,6 @@
+
+document.getElementById("openLogin").addEventListener("click", function() {
+  document.getElementById("login").style.display = "block";
+});
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault()});
