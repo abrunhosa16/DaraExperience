01a5a357490129872462c9eb6eaeba1ea3a75e3b
abrunhosa16 <146744014+abrunhosa16@users.noreply.github.com>
2023-11-06 03:05:44 +0000

    instructions and alignment

--- src/index.js
+++ src/index.js
@@ -9,6 +9,36 @@ function main() {
 
   const area = new BoardArea();
   area_target.parentElement.replaceChild(area.el(), area_target);
+
+  const openInstructionsButton = document.getElementById("openInstructions");
+  const instructionsModal = document.getElementById("instructionsModal");
+  const closeButton = document.querySelector(".close");
+
+  openInstructionsButton.addEventListener("click", function () {
+    instructionsModal.style.display = "block";
+  });
+
+  closeButton.addEventListener("click", function () {
+    instructionsModal.style.display = "none";
+  });
+
+  window.addEventListener("click", function (event) {
+    if (event.target === instructionsModal) {
+      instructionsModal.style.display = "none";
+    }
+  });
+
+  const loginButton = document.querySelector(".start-button");
+  const registrationForm = document.getElementById("registration-form");
+  const closeFormButton = document.getElementById("closeFormButton");
+
+  loginButton.addEventListener("click", function () {
+    registrationForm.style.display = "block";
+  });
+
+  closeFormButton.addEventListener("click", function () {
+    registrationForm.style.display = "none";
+  });
 }
 
 main();
