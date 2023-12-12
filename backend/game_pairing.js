1de333b4e19fd04f781b10ebc0b3acb76f06b77d
Zakhar Starinski <zakharstarinski17@gmail.com>
2023-12-12 02:44:44 +0000

    Added connecting to join in game pairing

--- backend/game_pairing.js
+++ backend/game_pairing.js
@@ -12,93 +12,81 @@ const SEARCH_TIMEOUT = 60000;
 //  times for the same parameters they are just going to refresh the timeouts.
 
 // There are four waiting states for a user:
-//  - Connecting: The user called /join and no pairings exited. 
-//      They received a new game id and will be forgotten if they not call 
+//  - Connecting: The user called /join and no pairings exited.
+//      They received a new game id and will be forgotten if they not call
 //      /update in the next <JOIN_TIMEOUT> ms.
-//  - Searching: The user was connected by calling /update while previously 
-//      "Connecting" and will wait for another user to /join. This state has a 
+//  - Searching: The user was connected by calling /update while previously
+//      "Connecting" and will wait for another user to /join. This state has a
 //      timeout of <SEARCH_TIMEOUT> ms in which a <no winners> message is sent
 //      and the user is expected to disconnect.
-//  - Paired: The user called /join and was automatically paired with a adequate player.
+//  - Paired: The user called /join and was automatically paired with an adequate player.
 //      They received the game id of the paired player and will be forgotten if
 //      they not call /update in the next <JOIN_TIMEOUT> ms. If the other user leaves,
-//      the state is changed to "Connecting" with the 
-//  - Pairing: The state was previously "Searching" and another player called 
+//      the state is changed to "Connecting" with the
+//  - Pairing: The state was previously "Searching" and another player called
 //      /join however hasn't called /update yet. In this state, no new pairings
-//      can be received. If the other user calls /update they will be 
-//      successfully paired and the game will start, otherwise the state will 
-//      change again to "Searching" (this also happens if the other user 
+//      can be received. If the other user calls /update they will be
+//      successfully paired and the game will start, otherwise the state will
+//      change again to "Searching" (this also happens if the other user
 //      specifically leaves).
 
 // Because /join is expected to return immediately, the only way for a user to
 //  get paired is to call the endpoint and another user be "Searching" with the
 //  adequate parameters. This means that multiple states can be "Searching" at
-//  the same time in witch case another user that calls /join will pair with 
+//  the same time in witch case another user that calls /join will pair with
 //  the oldest waiting one.
 // For example, if two users are "Connecting" at the same time and later
 //  successfully go to "Searching", only when a third user calls the endpoint
 //  again then a pairing will occur with the user that joined "Searching" first.
 
-
 export default class GamePairing {
   constructor() {
-    this.games = {};
+    this.connecting = {};
+    this.searching = {};
+    this.pairing = {};
 
-    // this.games cache
-    this.by_size = {};
+    // existing users and their specific size respective state
+    this.users = {};
+
+    // ids contained in this.searching for a particular size
+    this.searching_by_size = {};
   }
 
-  join(username, width, height, timeout, timeout_callback) {
-    if (this.by_username.hasOwnProperty(username)) {
-      return {
-        id: this.by_username[username],
-        paired: false,
-      };
+  // Gets called on endpoint /join
+  // Supposed to not fail (throw any errors)
+  // Restarts timeouts if called multiple times for the same parameters and
+  //  while in "Connecting" or "Pairing" states, otherwise just returns the
+  //  game id.
+  join(username, width, height) {
+    const users_key = `${username}-${width}-${height}`;
+    if (this.users.hasOwnProperty(users_key)) {
+      return; // todo
     }
 
-    if (this.by_size.hasOwnProperty(width)) {
-      const by_width = this.by_size[width];
-      if (by_width.hasOwnProperty(height)) {
-        const id = by_width[height];
-        const other = this.by_username[id];
-
-        // delete other
-        this.leave(id);
-
-        return {
-          id: id,
-          paired: true,
-          user1: other,
-          user2: username,
-        };
-      }
+    const size_key = `${width}-${height}`;
+    if (this.searching_by_size.hasOwnProperty(size_key)) {
+      // adequate pair found
+      // todo
     } else {
-      this.by_size[width] = {};
+      // no adequate pair -> add to this.connecting
+      const id = crypto.randomUUID();
+      this.connecting[id] = {
+        username: username,
+        width: width,
+        height: height,
+        timeout: setTimeout(() => {
+          this.leave(id);
+        }, JOIN_TIMEOUT),
+      };
+      this.users[users_key] = {
+        status: "Connecting",
+        id: id,
+      };
+      return id;
     }
-
-    const id = crypto.randomUUID();
-    this.by_id[id] = {
-      username: username,
-      width: width,
-      height: height,
-      timeout: setTimeout(() => {
-        this.leave(id);
-        if (timeout_callback !== undefined) {
-          timeout_callback();
-        }
-      }, timeout),
-    };
-    this.by_username = id;
-    this.by_size[width][height] = id;
-
-    console.log("join", this.by_id);
-
-    return {
-      id: id,
-      paired: false,
-    };
   }
 
+  // todo
   refreshTimeout(id, timeout, timeout_callback) {
     if (!this.by_id.hasOwnProperty(id)) {
       throw new Error("Invalid id");
@@ -112,6 +100,7 @@ export default class GamePairing {
     }, timeout);
   }
 
+  // todo
   leave(id) {
     if (!this.by_id.hasOwnProperty(id)) {
       throw new Error("Invalid id");
